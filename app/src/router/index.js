9a3514b4747004f5cbd0985d5c1a9a867891a77e
yanyinnicolewu <nicolewu288@gmail.com>
2026-02-12 12:34:34 -0500

    vcxv

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import icecreamstore from '@/views/icecreamstore.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: 
+  routes: [
+    {
+      path: '/icecreamstore',
+      name: 'icecreamstore',
+      component: icecreamstore,
+    },
+  ],
 })
 
 export default router
---
+++ app/src/views/icecreamstore.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container"></div>
+  <h1>Ice Cream DIY store</h1>
+  <img src="chocolate.png" alt="" />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const iceCreamMenu = ref([
+  { flavor: 'Vanilla', price: 3.99 },
+  { flavor: 'Chocolate', price: 4.99 },
+  { flavor: 'Strawberry', price: 4.49 },
+  { flavor: 'Mint Chocolate Chip', price: 5.49 },
+  { flavor: 'Cookies and Cream', price: 4.99 },
+  { flavor: 'Rocky Road', price: 5.99 },
+  { flavor: 'Butter Pecan', price: 5.49 },
+  { flavor: 'Coffee', price: 4.49 },
+  { flavor: 'Pistachio', price: 5.99 },
+  { flavor: 'Cookie Dough', price: 5.49 },
+  { flavor: 'Neapolitan', price: 2.97 },
+  { flavor: 'Chocolate Fudge Brownie', price: 7.99 },
+])
+</script>
+
+<style lang="scss" scoped></style>
